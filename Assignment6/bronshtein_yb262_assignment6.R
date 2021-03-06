95c95951a5d3be18468af023db93b1c6c2398b62
yanbronshtein <yanbronshtein@gmail.com>
2021-03-06 15:36:23 -0500

    Finished Problem 1 Assignment 6 data wrangling

--- Assignment6/bronshtein_yb262_assignment6.R
+++ Assignment6/bronshtein_yb262_assignment6.R
@@ -15,7 +15,6 @@ kidnapped_t <- gutenberg_download(c(421)) %>% as_tibble()
 tidy_treasure_island_t <- treasure_island_t %>%
   unnest_tokens(word, 
                 text, 
-                #to_lower = TRUE,#
                 token = "words") %>%
   anti_join(stop_words)
 
@@ -23,7 +22,6 @@ tidy_treasure_island_t <- treasure_island_t %>%
 tidy_kidnapped_t <- kidnapped_t %>%
   unnest_tokens(word, 
                 text, 
-                #to_lower = TRUE,#
                 token = "words") %>%
   anti_join(stop_words)
 
@@ -59,17 +57,18 @@ top10_words_kidnapped <- tidy_kidnapped_t %>%
 #of the non stop words (i.e., words that are not stop words) in the two books, 
 #and calculate the correlation between them.
 
-frequency <- bind_rows(mutate(tidy_treasure_island_t, title = "Treasure_Island"),
+frequency_t <- bind_rows(mutate(tidy_treasure_island_t, title = "Treasure_Island"),
                        mutate(tidy_kidnapped_t, title = "Kidnapped")) %>%
   mutate(word = str_extract(word, "[a-z']+")) %>%
   count(title, word) %>%
   group_by(title) %>%
   mutate(proportion = n / sum(n)) %>%
   select(-n) %>%
-  pivot_wider(names_from = "title", values_from = "proportion") %>%
-  filter(!(Treasure_Island=="NA"|Kidnapped=="NA")) #%>%
+  pivot_wider(names_from = "title", values_from = "proportion")
+  
+ #%>%
 
-ggplot(data = frequency, mapping = aes(x = Treasure_Island,
+ggplot(data = frequency_t %>% filter(!(Treasure_Island=="NA"|Kidnapped=="NA")), mapping = aes(x = Treasure_Island,
                       y = Kidnapped)) +
   geom_abline(color = "red", lty = 2,
               lwd=2) +
@@ -80,44 +79,48 @@ ggplot(data = frequency, mapping = aes(x = Treasure_Island,
   scale_y_log10()  
 
 
-corr <- frequency %>% select(-word) %>% cor()
+corr <- frequency_t %>% filter(!(Treasure_Island=="NA"|Kidnapped=="NA")) %>% select(-word) %>% cor()
 corr
 #(ii) Find two words that appear 
 #with a high frequency in Kidnapped but not in Treasure Island.
 high_kidnapped_low_treasure_freq <- 
-  bind_rows(mutate(tidy_treasure_island_t, title = "Treasure_Island"),
-                       mutate(tidy_kidnapped_t, title = "Kidnapped")) %>%
-  mutate(word = str_extract(word, "[a-z']+")) %>%
-  count(title, word) %>%
-  group_by(title) %>%
-  mutate(proportion = n / sum(n)) %>%
-  select(-n) %>%
-  pivot_wider(names_from = "title", values_from = "proportion") %>%
+  frequency_t %>%
   filter(Kidnapped != "NA") %>%
   arrange(desc(Kidnapped)) %>%
   filter(is.na(Treasure_Island)) %>%
-  head(2)
+  head(2) %>%
+  select(word)
 
 
 #(iii) Find two words that appear with a high frequency in Treasure Island 
 #but not in Kidnapped.
 high_treasure_low_kidnapped_freq <- 
-  bind_rows(mutate(tidy_treasure_island_t, title = "Treasure_Island"),
-            mutate(tidy_kidnapped_t, title = "Kidnapped")) %>%
-  mutate(word = str_extract(word, "[a-z']+")) %>%
-  count(title, word) %>%
-  group_by(title) %>%
-  mutate(proportion = n / sum(n)) %>%
-  select(-n) %>%
-  pivot_wider(names_from = "title", values_from = "proportion") %>%
+  frequency_t %>%
   filter(Treasure_Island != "NA") %>%
   arrange(desc(Treasure_Island)) %>%
   filter(is.na(Kidnapped)) %>%
-  head(2)
+  head(2) %>%
+  select(word)
 
 
 #(iv) Find two words that appear with high frequency in both novels.
+highest_freq_treasure_island <- 
+  frequency_t %>%
+  filter(Treasure_Island != "NA") %>%
+  arrange(desc(Treasure_Island)) %>%
+  select(word)
 
+highest_freq_kidnapped <- 
+  frequency_t %>%
+  filter(Kidnapped != "NA") %>%
+  arrange(desc(Kidnapped)) %>%
+  select(word)
+
+both_highest_freq <- 
+  inner_join(highest_freq_treasure_island, highest_freq_kidnapped) %>%
+  select(word) %>%
+  head(2)
+  
 #4.Find the 10 most common bigrams in Treasure Island that do not include stop words.
 
 #5. Plot the sentiment for the two books using the bing lexicon, 
